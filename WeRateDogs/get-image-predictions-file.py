5cc0ff31151f21e5846df4dc097954b53d53d0a7
Paul Smith <paul@numruff.com>
2022-02-26 00:53:40 -0800

    Get image-predictions.tsv file from provided URL.

---
+++ WeRateDogs/get-image-predictions-file.py
@@ -0,0 +1,23 @@
+import os
+import posixpath
+import requests
+from urllib.parse import urlsplit, unquote
+
+# Download image_predictions.tsv via requests from the provided URL.
+
+# Provided URL.
+url = "https://d17h27t6h515a5.cloudfront.net/topher/2017/August/599fd2ad_image-predictions/image-predictions.tsv"
+
+# Extract file name.
+path = urlsplit(url).path
+file_name = posixpath.basename(unquote(path))
+
+# Get data from URL via requests library.
+url_data = requests.get(url)
+
+# If file exists, do nothing, else write the data to a file.
+if not os.path.isfile(file_name):
+    with open(file_name, 'wb') as file:
+        file.write(url_data.content)
+else:
+    print("%s file exists." % file_name)
